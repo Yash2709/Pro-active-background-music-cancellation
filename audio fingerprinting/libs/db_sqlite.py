bea80663ebceba3717a3c8eb4a0cbcfc157c0da6
Yash <41947298+Yash2709@users.noreply.github.com>
2019-12-06 23:09:09 +0000

    Added queries model

--- audio fingerprinting/libs/db_sqlite.py
+++ audio fingerprinting/libs/db_sqlite.py
@@ -27,3 +27,33 @@ class SqliteDatabase(Database):
     self.conn.close()
     print(colored('sqlite - connection has been closed','white',attrs=['dark']))
 
+  def query(self, query, values = []):
+    self.cur.execute(query, values)
+
+  def executeOne(self, query, values = []):
+    self.cur.execute(query, values)
+    return self.cur.fetchone()
+
+  def executeAll(self, query, values = []):
+    self.cur.execute(query, values)
+    return self.cur.fetchall()
+
+  def buildSelectQuery(self, table, params):
+    conditions = []
+    values = []
+
+    for k, v in enumerate(params):
+      key = v
+      value = params[v]
+      conditions.append("%s = ?" % key)
+      values.append(value)
+
+    conditions = ' AND '.join(conditions)
+    query = "SELECT * FROM %s WHERE %s" % (table, conditions)
+
+    return {
+      "query": query,
+      "values": values
+    }
+
+ 
